5afd0228b20772606914cc55803ec187366e1b2d
thomasbirien <thomasbirien@gmail.com>
2017-07-01 17:27:04 +0200

    suppretion of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
 
-  get 'activities/index'
-
-  get 'activities/show'
-
-  get 'activities/new'
-
-  get 'activities/create'
-
-  get 'activities/edit'
-
-  get 'activities/update'
-
-  get 'activities/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
