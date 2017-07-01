a7576856998ee6eacf9282d227f380b73b3a3eb9
thomasbirien <thomasbirien@gmail.com>
2017-07-01 17:47:29 +0200

    writed basics method for Activities controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,22 +1,50 @@
 class ActivitiesController < ApplicationController
   def index
+    @activities = Activity.all
   end
 
   def show
+    @activity = Activity.find(params[:id])
   end
 
   def new
+    @activity = Activity.new
   end
 
   def create
+    @activity = Activity.new(activity_params)
+    if @activity.save
+      redirect_to activity_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @activity = Activity.find(params[:id])
   end
 
   def update
+    @activity = Activity.find(params[:id])
+    if @activity.update(activity_params)
+      redirect_to activity_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @activity = Activity.find(params[:id])
+    @activity.destroy
+    redirect_to activity_path
+  end
+
+  private
+  def activity_params
+    params.require(:activity).permit(
+      :tank_name,
+      :quantity,
+      :quantity_max
+      )
   end
 end
