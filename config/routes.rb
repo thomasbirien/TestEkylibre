dc454263b5b4e19c48497178a709f26e1a5927a1
thomasbirien <thomasbirien@gmail.com>
2017-07-01 17:51:46 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'tanks#index'
 
+  resources :tanks do
+    resources :activities
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
