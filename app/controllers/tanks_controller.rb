15c0eff3fac5478407fe4c6d50e0a0318a8f9148
thomasbirien <thomasbirien@gmail.com>
2017-07-01 17:44:24 +0200

    writed basics method for CRUD

--- app/controllers/tanks_controller.rb
+++ app/controllers/tanks_controller.rb
@@ -1,22 +1,50 @@
 class TanksController < ApplicationController
   def index
+    @tanks = Tank.all
   end
 
   def show
+    @tank = Tank.find(params[:id])
   end
 
   def new
+    @tank = Tank.new
   end
 
   def create
+    @tank = Tank.new(tank_params)
+    if @tank.save
+      redirect_to tank_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @tank = Tank.find(params[:id])
   end
 
   def update
+    @tank = Tank.find(params[:id])
+    if @tank.update(tank_params)
+      redirect_to tank_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @tank = Tank.find(params[:id])
+    @tank.destroy
+    redirect_to tank_path
+  end
+
+  private
+  def tank_params
+    params.require(:tank).permit(
+      :tank_name,
+      :quantity,
+      :quantity_max
+      )
   end
 end
