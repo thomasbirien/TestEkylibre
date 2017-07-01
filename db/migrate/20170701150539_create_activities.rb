581ae3b7e87cabc00adb361d54465c614a464276
thomasbirien <thomasbirien@gmail.com>
2017-07-01 17:13:24 +0200

    correction migration

--- db/migrate/20170701150539_create_activities.rb
+++ db/migrate/20170701150539_create_activities.rb
@@ -2,7 +2,7 @@ class CreateActivities < ActiveRecord::Migration[5.0]
   def change
     create_table :activities do |t|
       t.string :intervention
-      t.references :Tank, foreign_key: true
+      t.references :tank, foreign_key: true
 
       t.timestamps
     end
